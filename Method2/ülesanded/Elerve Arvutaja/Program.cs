20dddf003ed96f2a2cb6043e6e21b114b88e9053
Opilane TTHK <opilane@tthk.ee>
2025-11-28 15:52:46 +0200

    nedodelono

---
+++ Method2/ülesanded/Elerve Arvutaja/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Elerve_Arvutaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //nimi
+            string kasutajaNimi = "";
+            while (kasutajaNimi == "")
+            {
+                Console.WriteLine("Tere, sisesta om nimi");
+                kasutajaNimi = Console.ReadLine();
+            }
+
+            //küsi kasutajalt tema tulisid, niikaua kuni rohkempole
+            string sisestus = "";
+            List<float> tulud = new List<float>();
+            do
+            {
+                Console.WriteLine("sisesta om tulud *ÜKSHAAVAL* - järgmine tulu:");
+                sisestus = Console.ReadLine();
+
+                float conversion = float.Parse(sisestus);
+                tulud.Add(conversion);
+
+            } while (sisestus != "rohkem pole");
+            Console.WriteLine(sisestus);
+
+            //siis
+            //string sisestus = "";
+            //List<float> tulud = v
+
+            //    //
+            if (sisestus == "")
+                arvutakokku(tulud);
+            //    //
+            //    //
+            //    //
+            //    //
+            //    //
+            //    //
+
+
+
+        }
+
+        private static void arvutakokku(List<float> tulud)
+        {
+            float kokku = 0;
+            for (int i = 0; i < tulud.Count; i++)
+            {
+                kokku += tulud[i];
+            }
+        }
+    }
+}
