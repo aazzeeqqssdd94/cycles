c60a6196d5d22d2760eea5d29cdebe4c2e93cbba
Opilane TTHK <opilane@tthk.ee>
2025-11-28 14:30:32 +0200

    ühe arvu teise andme

---
+++ Method2/Casting/Casting/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Channels;
+
+namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int arv = 67;
+            string arvtekstina = arv.ToString();
+            Console.WriteLine(arvtekstina);
+            Console.WriteLine(arv);
+            double arvdouble = 7236457.862379d;
+            int arvdoublearvint = 0;
+            arvdoublearvint = (int)arvdouble;
+
+            double uusdouble = arv;
+
+            //kirjuta programm mis küsib kasutajalt meetrites tema pikkust (komakohtaga)
+            string pikkus = "";
+            Console.WriteLine("sisesta pikkus:");
+            pikkus = Console.ReadLine();
+            float pikkusFm = float.Parse(pikkus);
+            int pikkusCm = (int)(pikkusFm*100);
+            Console.WriteLine($"Sinu pikkus CMides on {pikkusCm}, ja meetrites {pikkusFm}");
+
+
+            //teisenda kasutaja pikkus sentimeetritek
+
+            float arv1 = 0.45f;
+            float arv2 = 0.55f;
+            Console.WriteLine(arv1);
+            Console.WriteLine(arv2);
+            Console.WriteLine((int)arv1);
+            Console.WriteLine((int)arv2);
+            Console.WriteLine((int)Math.Round(arv1));
+            Console.WriteLine((int)Math.Round(arv2));
+        }
+    }
+}
