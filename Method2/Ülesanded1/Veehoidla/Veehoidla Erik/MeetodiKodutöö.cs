006eac65c82a23bd51687a0d6521300b8dd394bd
Opilane TTHK <opilane@tthk.ee>
2025-12-12 15:29:35 +0200

    123

--- Method2/Ülesanded1/Veehoidla/Veehoidla Erik/MeetodiKodutöö.cs
+++ Method2/Ülesanded1/Veehoidla/Veehoidla Erik/MeetodiKodutöö.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Veehoidla_Erik
 {
-    internal class MeetodiKodutöö
+    public class MeetodiKodutöö
     {
+        public MeetodiKodutöö()
+        {
+            //1. ülesanne
+            // kirjuta meetod mis tervitab kasutajat
+            // kirjuta meetod mis ütleb talle headaega
+            // meetodeid kasutatakse programmis, mis apelsin ananass, kus kõigepealt tsüklis kasutajalt tema nime
+            // nimi salvestatakse muutuja antakse kaasa meetoditele. meetodid ise midagi ei tagasta.
+
+            //2. ülesanne
+            //kirjuta meetod, mis joonistab käsureale ruudu, võtab parameetrik kas täisarvu, (x ja y) ja konsoolivärvi.
+            //kirjuta teine meetod, mis vahetab konsooli värvi ja võtab parameetriks kasutaja sisestuse
+            // meetodeid kasutatakse programmsi, kus, abrakadabra, küsitakse kasutajalt tema lemmikvärvi, teie meetodi abil
+            // teise meetodi tulemus hoitakse muutujas, ning see antakse kaasa eimesele meetodile, mis joonistab ruud aga õige värviga
+
+            // varasemate ülesannete osi, saab siin taaskasutada.
+        }
     }
 }
