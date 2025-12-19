1afc3bb587fa814ca38cd04c441156afa70b935e
peskybirb <116211837+peskybirb@users.noreply.github.com>
2025-12-19 14:24:19 +0200

    mängudküsia

---
+++ Strings/sõneÜlesanne1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+
+namespace sõneÜlesanne1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kirjuta programm mis
+            //küsib kasutajalt tsüklis, millised mängud talle meeldivad
+            List<string> kasutajaMängud = new List<string>();
+
+            string mängunimi = Console.ReadLine();
+            Console.WriteLine("Millise mängud sulle meeldivad?");
+
+            while (mängunimi != "ei ole")
+            {
+                Console.WriteLine("palun sisesta järgmine mäng, kui rohkem ei ole, kirjuta \"ei ole\"");
+                mängunimi = Console.ReadLine();
+                if (mängunimi != "ei ole")
+                {
+                    //küsimiste tulemus hoia järjendis
+                    kasutajaMängud.Add(mängunimi);
+                }
+            }
+
+            //kui järjend sisaldab (.Contains()) "mario kart", siis küsi kui vana ta on, sarkastiliselt.
+
+            if (kasutajaMängud.Contains("mario kart"))
+            {
+                Console.WriteLine("MIKS SA MARIO KARTI MÄNGID, MINGI 5 OLED VÄ!??!?!");
+            }
+
+            //kui järjend sisaldab "gta 6", siis küsi kust ta ajamasina sai, et rockstar lõpuks asjaga valmis sai
+
+            if (kasutajaMängud.Contains("gta 6"))
+            {
+                Console.WriteLine("kust ajamasina said? rockstar sellega juba valmis sai? o_Õ");
+            }
+
+            //kui järjend sisaldab "pong", siis ütle "80ndad tahavad vanureid tagasi, toimub boomerite recall"
+
+            if (kasutajaMängud.Contains("pong"))
+            {
+                Console.WriteLine("80ndad tahavad vanureid tagasi, toimub boomerite recall");
+            }
+
+            //kui järjend on tühi, siis programm läheb edasi ->
+
+            if (!kasutajaMängud.Any())    //Any ничего
+            {
+                Console.WriteLine("kas sulle ei meeldi mängud? vasta kas jah või ei:");
+                string jahvõiei = Console.ReadLine().ToLower();
+                //kui ta vastab jah, siis ütle, "aga miks siis ei sisestanud?"
+                if (jahvõiei.Contains("jah"))
+                {
+                    Console.WriteLine("aga miks siis ei sisestanud?");
+                }
+                //kui talle ei meeldi, siis ütle, kahju
+                else if (jahvõiei.Contains("ei"))
+                {
+                    Console.WriteLine("kahju :c");
+                }
+                //kui vastus on midagi muud, siis ütle "ei saa aru :/".
+                else
+                {
+                    Console.WriteLine("ei saa aru :/");
+                }
+                
+                
+                //kui vastus on midagi muud, siis ütle "ei saa aru :/".
+            }
+
+            //küsi kasutajalt kas talle ei meeldi mängud, ning löase tal vastata sõnaga jah või ei, kontrolli kasutaja sisestust .ToUpper() või .ToLower()iga
+
+
+
+
+        }
+    }
+}
