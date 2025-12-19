35d80436c9c00434555844e56e038ec3f495cf65
peskybirb <116211837+peskybirb@users.noreply.github.com>
2025-12-19 13:33:39 +0200

    strings

---
+++ Strings/string tools/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace string_tools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lause = "täNa hommikul jõin äras terve ploki monsterit, aga üles ei ärganud, tahan veel magada";
+            //Console.WriteLine(lause.Length);      //количество символов
+            //Console.WriteLine(lause);
+            //Console.WriteLine(lause.ToUpper());   //делает буквы большыми
+            //Console.WriteLine(lause.ToLower());   //делает буквы большыми
+            //Console.WriteLine("kas lauses on sõna \"monster\"?:"+lause.ToLower().Contains("monsterit"));
+
+            //List<string> sõnenimekiri = new List<string>{"auto", "kiirabi", "tuletõrje" };
+            //string kogunimekiri = "";
+            //foreach (var sõna in sõnenimekiri)
+            //{
+            //    kogunimekiri += (" "+sõna.ToUpper()+",");
+            //}
+            //Console.WriteLine(kogunimekiri);
+
+            //string eesnimi = "julius";
+            //string perekonnanimi = "Caesar";
+            //string amet = "Noahoidik";
+            //string kuuluskeiser = string.Concat(eesnimi, perekonnanimi, amet);
+            //Console.WriteLine(kuuluskeiser);
+
+            //string kuuluskeiser2 = $"{eesnimi} {perekonnanimi} oli maailma kuulsaim {amet}";
+            //string kuuluskeiser3 = "" + eesnimi + "  " + perekonnanimi + " oli maailma kuulsain " + amet;
+            //Console.WriteLine(kuuluskeiser3);
+            //Console.WriteLine(kuuluskeiser2);
+
+            //повтор
+
+            //string[] kõiksõnadlauses = lause.Split(' ');
+            //foreach (var sõnainlause in kõiksõnadlauses)
+            //{
+            //    Console.WriteLine(sõnainlause);
+            //}
+
+            //string üksõna = "mustalehmatagavalgelehmasaba";
+            //Console.WriteLine(üksõna[13]);                      //выводит букву по её наъождению
+            //Console.WriteLine(üksõna.IndexOf("l"));
+
+            //Console.WriteLine(" \" ");
+            //Console.WriteLine("a \n b");     //пишет на новой строчке
+            //Console.WriteLine("AAA\tBBBBB"); //делает большой пробел
+
+        }
+    }
+}
